6a5e68e62a03d4c4dc200659681036d8ca9eba59
touahriayacine <medyacinetouahria@gmail.com>
2023-05-08 14:07:18 +0100

    converting frontendfrom php to html

--- script/index.js
+++ script/index.js
@@ -3,3 +3,20 @@ function startFromTheEnd() {
     chats.scrollTo(0, chats.scrollHeight);
 }
 startFromTheEnd();
+
+
+function configCancelButton() {
+    let cancel_btn = document.getElementById("cancel");
+    let form_container = document.getElementById("form-container");
+    cancel_btn.addEventListener("click", () => {
+        form_container.style = "display:none";
+    });
+}
+
+configCancelButton();
+
+
+function showFilter() {
+    let form_container = document.getElementById("form-container");
+    form_container.style = "display:flex";
+}
